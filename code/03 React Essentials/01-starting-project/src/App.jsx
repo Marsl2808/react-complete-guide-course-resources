8c723a645cf94768f3b423c376c3bfeb9a9dbdb5
Marslinho <m.wendling2808@gmx.de>
2024-11-03 11:05:05 +0100

    finished react essentials (*/code/03 React Essentials/01-starting-project)

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -9,9 +9,8 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/Coreconcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
-
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -26,7 +25,8 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map(conceptItem => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               img={CORE_CONCEPTS[0].image}
@@ -41,7 +41,7 @@ function App() {
               title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
               img={CORE_CONCEPTS[3].image}
-            />
+            /> */}
           </ul>
         </section>
 
@@ -50,26 +50,53 @@ function App() {
           <h2>Examples</h2>
           {/* menu -> create list of buttons */}
           <menu>
-            <TabButton click={() => handleClick("components")}>
+            <TabButton
+              click={() => handleClick("components")}
+              isSelected={selectedTopic === "components"}
+            >
               {CORE_CONCEPTS[0].title}
             </TabButton>
-            <TabButton click={() => handleClick("jsx")}>
+            <TabButton
+              click={() => handleClick("jsx")}
+              isSelected={selectedTopic === "jsx"}
+            >
               {CORE_CONCEPTS[1].title}
             </TabButton>
-            <TabButton click={() => handleClick("props")}>
+            <TabButton
+              click={() => handleClick("props")}
+              isSelected={selectedTopic === "props"}
+            >
               {CORE_CONCEPTS[2].title}
             </TabButton>
-            <TabButton click={() => handleClick("state")}>
+            <TabButton
+              click={() => handleClick("state")}
+              isSelected={selectedTopic === "state"}
+            >
               {CORE_CONCEPTS[3].title}
             </TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+
+          {/* conditional output */}
+          {/* !selectedTopic same as: selectedTopic === undefined*/}
+          {!selectedTopic ? <p>Please select a topic.</p> : null}
+          {/* selectedTopic same as: selectedTopic !== undefined*/}
+          {/* alternative: second condition here can also be the default (null) case of the first*/}
+          {/* alternative: &&-operator => will output 2. statement if first is true */}
+          {/* alternative: set variable to <p>-statement & overwrite var in if statement, then simply output var here: {var} */}
+          {selectedTopic &&
+            <div id="tab-content">
+              <h3>
+                {EXAMPLES[selectedTopic].title}
+              </h3>
+              <p>
+                {EXAMPLES[selectedTopic].description}
+              </p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>}
         </section>
       </main>
     </div>
